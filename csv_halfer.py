5e25e82ead3078bb57fe696a29dcea6fecf97e23
Alejandro <alejandrougas32@gmail.com>
2025-04-09 14:50:43 -0400

    new halfer helper script

---
+++ csv_halfer.py
@@ -0,0 +1,25 @@
+"""
+used to 0.5x the OSM data set, custom written
+"""
+
+from tqdm import tqdm
+
+original_file = "osm_na_node_data.csv"
+new_file = "osm_na_node_data_half.csv"
+
+#get the total number of lines
+with open(original_file, 'r', encoding='utf-8', errors='ignore') as src:
+    total_lines = sum(1 for _ in src) - 1  
+
+#get half the lines
+half_lines = total_lines // 2
+
+with open(original_file, 'r', encoding='utf-8', errors='ignore') as src:
+    header = src.readline()
+
+    with open(new_file, 'w', encoding='utf-8') as dst:
+        dst.write(header)
+        for i, line in enumerate(tqdm(src, total=half_lines, desc="Writing 50%")):
+            if i >= half_lines:
+                break
+            dst.write(line)
